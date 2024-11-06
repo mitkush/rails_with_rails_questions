5bbecfc757b54d4e17768df1007770846d9e5221
mitkush <mitanshukushwah@gmail.com>
2024-11-06 18:03:16 +0530

    fixed the js of bootstrap configuration

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import Welcome from './components/Welcome';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDom.createRoot(document.getElementById('welcome'));
 root.render(<Welcome />);
