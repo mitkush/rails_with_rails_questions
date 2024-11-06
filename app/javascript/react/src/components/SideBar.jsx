81843764e629fed4bb11e50277863a811609757f
mitkush <mitanshukushwah@gmail.com>
2024-11-06 14:49:01 +0530

    added bootstrap icons

--- app/javascript/react/src/components/SideBar.jsx
+++ app/javascript/react/src/components/SideBar.jsx
@@ -22,15 +22,17 @@ const SideBar = () => {
             </a>
           </li>
         </ul>
-        <hr />
-        <div className="dropdown pb-4">
-          <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle" />
-            <span className="d-none d-sm-inline mx-1">loser</span>
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a className="dropdown-item" href="#">Sign out</a></li>
-          </ul>
+
+        <div className="mt-auto">
+          <div className="dropdown pb-4">
+            <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle" />
+              <span className="d-none d-sm-inline mx-1">loser</span>
+            </a>
+            <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+              <li><a className="dropdown-item" href="#">Sign out</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
