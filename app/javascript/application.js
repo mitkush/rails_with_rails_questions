0528e496a73a1be50db74e3ed7a7cd8e439e4a3d
mitkush <mitanshukushwah@gmail.com>
2024-11-07 13:22:20 +0530

    resolve all bootstrap js related issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@
 // import "@hotwired/turbo-rails"
 // import "./controllers"
 import './react/src/index.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import Welcome from './components/Welcome';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDom.createRoot(document.getElementById('welcome'));
 root.render(<Welcome />);
